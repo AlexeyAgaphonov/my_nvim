da8c76a377df4337445cfa5efabf211b74610cc8
Aleksei Agafonov <a.agafonov@melsoft-games.com>
2025-05-07 16:47:58 +0300

    markdown settings

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,7 +4,7 @@ return {
     config = function()      
         local configs = require("nvim-treesitter.configs")
         configs.setup({
-            ensure_installed = { "lua", },
+            ensure_installed = { "lua", "markdown", "markdown_inline"},
             highlight = { enable = true },
             indent = { enable = true },
         })
--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -5,3 +5,9 @@ vim.cmd("set shiftwidth=4")
 vim.cmd("set number")
 vim.g.mapleader = " "
 
+vim.api.nvim_create_autocmd({"BufNewFile", "BufRead"}, {
+    pattern = {"*.gui_script", "*.script", "*.render_script"},
+    callback = function ()
+        vim.bo.filetype = "lua"
+    end
+})
